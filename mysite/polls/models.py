fe243daf788ce1594f88d87e27cd89eb4f455ed7
eddyeboy27 <lipe0503@gmail.com>
2020-10-19 05:21:31 -0300

    criação das tabelas no models, Question e Choice

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+# As classes definem as tabelas e as variaveis as colunas da tabela
+
+class Question(models.Model):
+  question_text = models.CharField(max_length=200)
+  pub_date = models.DateTimeField('data published')
+
+
+class Choice(models.Model):
+  question = models.ForeignKey(Question, on_delete=models.CASCADE)
+  choice_text = models.CharField(max_length=200)
+  votes = models.IntegerField(default=0)
+
 # Create your models here.
